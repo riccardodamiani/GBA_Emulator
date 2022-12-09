24c8990b2783cb10612a3d5a454ac47d8da19c2b
Riccardo Damiani <riccardodamiani.28@gmail.com>
2022-12-09 21:45:22 +0100

    set up thumb decoder

--- thumb_decoder.cpp
+++ thumb_decoder.cpp
@@ -7,11 +7,11 @@ THUMB_opcode ThumbDecoder::decode(uint16_t opcode) {
 	case 0:
 		//move shifted register
 		//add/subtract
-		return THUMB_OP_INVALID;
+		return decode_0(opcode);
 		break;
 	case 1:
 		//move/compare/add/subtract immidiate
-		return THUMB_OP_INVALID;
+		return decode_1(opcode);
 		break;
 	case 2:
 		//alu operation
@@ -19,67 +19,86 @@ THUMB_opcode ThumbDecoder::decode(uint16_t opcode) {
 		//load pc relative
 		//load/store with register offset
 		//load store sign-extended byte/halfword
-		return THUMB_OP_INVALID;
+		return decode_2(opcode);
 		break;
 	case 3:
 		//load store with immidiate offset
-		return THUMB_OP_INVALID;
+		return decode_3(opcode);
 		break;
 	case 4:
 		//load store halfword
 		//load/store sp-relative
-		return THUMB_OP_INVALID;
+		return decode_4(opcode);
 		break;
 	case 5:
 		//get relative address
 		//add offset stack pointer
 		//push pop
-		return THUMB_OP_INVALID;
+		return decode_5(opcode);
 		break;
 	case 6:
 		//multiple load/store
 		//conditional branch
 		//software interrupt
-		return THUMB_OP_INVALID;
+		return decode_6(opcode);
 		break;
 	case 7:
 		//unconditional branch
 		//long branch with link
-		return THUMB_OP_INVALID;
+		return decode_7(opcode);
 		break;
 	}
 	return THUMB_OP_INVALID;
 }
 
+//move shifted register
+//add/subtract
 THUMB_opcode ThumbDecoder::decode_0(uint16_t opcode) {
 	return THUMB_OP_INVALID;
 }
 
+//move/compare/add/subtract immidiate
 THUMB_opcode ThumbDecoder::decode_1(uint16_t opcode) {
 	return THUMB_OP_INVALID;
 }
 
+//alu operation
+//hi reg operation/branch exchange
+//load pc relative
+//load/store with register offset
+//load store sign-extended byte/halfword
 THUMB_opcode ThumbDecoder::decode_2(uint16_t opcode) {
 	return THUMB_OP_INVALID;
 }
 
+//load store with immidiate offset
 THUMB_opcode ThumbDecoder::decode_3(uint16_t opcode) {
 	return THUMB_OP_INVALID;
 }
 
 
+//load store halfword
+//load/store sp-relative
 THUMB_opcode ThumbDecoder::decode_4(uint16_t opcode) {
 	return THUMB_OP_INVALID;
 }
 
+//get relative address
+//add offset stack pointer
+//push pop
 THUMB_opcode ThumbDecoder::decode_5(uint16_t opcode) {
 	return THUMB_OP_INVALID;
 }
 
+//multiple load/store
+//conditional branch
+//software interrupt
 THUMB_opcode ThumbDecoder::decode_6(uint16_t opcode) {
 	return THUMB_OP_INVALID;
 }
 
+//unconditional branch
+//long branch with link
 THUMB_opcode ThumbDecoder::decode_7(uint16_t opcode) {
 	return THUMB_OP_INVALID;
 }
